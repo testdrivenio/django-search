474e9b2012cca520f831e38a2947ebf5e5776af7
Samuel Torimiro <efosonaname@gmail.com>
2021-10-05 20:53:08 +0100

    add TrigramSimilarity & GinIndex (for speed)

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline, TrigramSimilarity
 from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
@@ -14,69 +14,9 @@ class QuoteList(ListView):
     template_name = "quote.html"
 
 
-# class SearchResultsList(ListView):
-#     model = Quote
-#     context_object_name = "quotes"
-#     template_name = "search.html"
+""" Q objects """
 
 
-# Q objects
-# class SearchResultsList(ListView):
-#     model = Quote
-#     context_object_name = "quotes"
-#     template_name = "search.html"
-
-#     def get_queryset(self):
-#         query = self.request.GET.get("q")
-#         return Quote.objects.filter(
-#             Q(name__icontains=query) | Q(quote__icontains=query)
-#         )
-
-
-# Single Field Search
-# class SearchResultsList(ListView):
-#     model = Quote
-#     context_object_name = "quotes"
-#     template_name = "search.html"
-
-#     def get_queryset(self):
-#         query = self.request.GET.get("q")
-#         return Quote.objects.filter(quote__search=query)
-
-
-# Multi Field Search
-# class SearchResultsList(ListView):
-#     model = Quote
-#     context_object_name = "quotes"
-#     template_name = "search.html"
-
-#     def get_queryset(self):
-#         query = self.request.GET.get("q")
-#         return Quote.objects.annotate(search=SearchVector("name", "quote")).filter(
-#             search=query
-#         )
-
-
-# Stemming and Ranking
-# class SearchResultsList(ListView):
-#     model = Quote
-#     context_object_name = "quotes"
-#     template_name = "search.html"
-
-#     def get_queryset(self):
-#         query = self.request.GET.get("q")
-#         search_vector = SearchVector("name", "quote")
-#         search_query = SearchQuery(query)
-#         return (
-#             Quote.objects.annotate(
-#                 search=search_vector, rank=SearchRank(search_vector, search_query)
-#             )
-#             .filter(search=search_query)
-#             .order_by("-rank")
-#         )
-
-
-# Weights
 class SearchResultsList(ListView):
     model = Quote
     context_object_name = "quotes"
@@ -84,12 +24,101 @@ class SearchResultsList(ListView):
 
     def get_queryset(self):
         query = self.request.GET.get("q")
-        search_vector = SearchVector("name", weight="B") + SearchVector(
-            "quote", weight="A"
+
+        return Quote.objects.filter(
+            Q(name__icontains=query) | Q(quote__icontains=query)
         )
-        search_query = SearchQuery(query)
-        return (
-            Quote.objects.annotate(rank=SearchRank(search_vector, search_query))
-            .filter(rank__gte=0.3)
-            .order_by("-rank")
-        )
+
+
+# Single Field Search
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         return Quote.objects.filter(quote__search=query)
+
+
+# Multi Field Search
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         return Quote.objects.annotate(
+#             search=SearchVector('name', 'quote')
+#         ).filter(search=query)
+
+
+# Stemming and Ranking
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         search_vector = SearchVector('name', 'quote')
+#         search_query = SearchQuery(query)
+#         return Quote.objects.annotate(
+#             search=search_vector,
+#             rank=SearchRank(search_vector, search_query)
+#         ).filter(search=search_query).order_by('-rank')
+
+
+# Adding Weights
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+
+#         query = self.request.GET.get('q')
+#         search_vector = SearchVector(
+#             'name', weight='B') + SearchVector('quote', weight='A')
+#         search_query = SearchQuery(query)
+
+
+#         return Quote.objects.annotate(
+#             rank=SearchRank(search_vector, search_query)
+#         ).filter(rank__gte=0.3).order_by('-rank')
+
+
+# Search Headline
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         search_vector = SearchVector('name', 'quote')
+#         search_query = SearchQuery(query)
+#         search_headline = SearchHeadline('quote', search_query)
+#         return Quote.objects.annotate(
+#             search=search_vector,
+#             rank=SearchRank(search_vector, search_query)
+#         ).annotate(headline=search_headline).filter(search=search_query).order_by('-rank')
+
+
+# Trigram Similarity
+# class SearchResultsList(ListView):
+#     model = Quote
+#     context_object_name = 'quotes'
+#     template_name = 'search.html'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+
+#         print(Quote.objects.annotate(
+#             similarity=TrigramSimilarity('quote', query),
+#         ).filter(similarity__gte=0.1).order_by('-similarity').explain(analyze=True))
+
+#         return Quote.objects.annotate(
+#             similarity=TrigramSimilarity('quote', query),
+#         ).filter(similarity__gte=0.1).order_by('-similarity')
