7e9fc99c699eca33a2885eb276fdbf95479452f7
Michael Herman <hermanmu@gmail.com>
2021-10-10 18:48:15 -0500

    add full text search

--- quotes/models.py
+++ quotes/models.py
@@ -1,9 +1,17 @@
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField
 from django.db import models
 
 
 class Quote(models.Model):
     name = models.CharField(max_length=250)
     quote = models.TextField(max_length=1000)
+    search_vector = SearchVectorField(null=True)
 
     def __str__(self):
         return self.quote
+
+    class Meta:
+        indexes = [
+            GinIndex(fields=["search_vector"]),
+        ]
